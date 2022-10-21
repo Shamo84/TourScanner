8962d869034953e7a840d8f9cd5b90df884a8ce6
Andrea Pacifico <pacifico.mensa@gmail.com>
2022-10-21 17:34:24 +0200

    prosegue

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { FaArrowRight } from 'react-icons/fa';
 
-export default function Card() {
-  return <div>Gallery</div>;
+export default function Card({ title, url }) {
+  return (
+    <div className='card'>
+      <img src={url} alt='no img' />
+      <div id='img-text'>
+        <FaArrowRight />
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
 }
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './Card';
+import axios from 'axios';
 
 export default function Gallery() {
-  return <div>Gallery</div>;
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://tourscanner.com/interview/images`).then(res => {
+      const posts = res.data;
+      setCats(posts);
+      console.log(posts);
+    });
+  }, []);
+
+  console.log(cats);
+
+  return (
+    <div id='kitten-gallery'>
+      <div className='container'>
+        {cats.map(cat => {
+          return <Card {...cat} key={cat.image_id} />;
+        })}
+      </div>
+    </div>
+  );
 }
